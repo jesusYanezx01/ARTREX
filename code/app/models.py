af0d5e121605f6d8fbbbce0bb5978162a2c35d36
Jesus Arturo Yañez Patiño <jesusyanezx01@gmail.com>
2024-09-15 20:21:45 -0500

    implementation of the entire database schema

--- code/app/models.py
+++ code/app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -6,10 +7,115 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    comments = db.relationship('Comment', backref='user', lazy=True)
+    scores = db.relationship('Score', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.username}>'
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    learning_paths = db.relationship('LearningPath', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+class Level(db.Model):
+    __tablename__ = 'levels'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    learning_paths = db.relationship('LearningPath', backref='level', lazy=True)
+
+    def __repr__(self):
+        return f'<Level {self.name}>'
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_comment = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    learning_path_id = db.Column(db.Integer, db.ForeignKey('learning_paths.id'), nullable=False)
+    
+    def __repr__(self):
+        return f'<Comment {self.id}>'
+
+class Score(db.Model):
+    __tablename__ = 'scores'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    learning_path_id = db.Column(db.Integer, db.ForeignKey('learning_paths.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Score {self.id}>'
+
+class LearningPath(db.Model):
+    __tablename__ = 'learning_paths'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    level_id = db.Column(db.Integer, db.ForeignKey('levels.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    comments = db.relationship('Comment', backref='learning_path', lazy=True)
+    resources = db.relationship('Resource', backref='learning_path', lazy=True)
+    labels = db.relationship('Label', secondary='learning_path_labels', lazy='subquery',
+                             backref=db.backref('learning_paths', lazy=True))
+
+    def __repr__(self):
+        return f'<LearningPath {self.title}>'
+
+class Resource(db.Model):
+    __tablename__ = 'resources'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text)
+    learning_path_id = db.Column(db.Integer, db.ForeignKey('learning_paths.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Resource {self.title}>'
+
+class Label(db.Model):
+    __tablename__ = 'labels'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f'<Label {self.name}>'
+
+class LearningPathLabel(db.Model):
+    __tablename__ = 'learning_path_labels'
+    
+    learning_path_id = db.Column(db.Integer, db.ForeignKey('learning_paths.id'), primary_key=True)
+    label_id = db.Column(db.Integer, db.ForeignKey('labels.id'), primary_key=True)
+    
+    def __repr__(self):
+        return f'<LearningPathLabel {self.learning_path_id}-{self.label_id}>'
